0dbb5567ae9a8b0d85614f442f175136fe547076
esjastad <esjastad@gmail.com>
2019-02-22 17:08:06 -0800

    Update QuickHull.py

--- QuickHull.py
+++ QuickHull.py
@@ -4,6 +4,8 @@ import random
 import copy
 import time
 import tkinter as tk
+import math 
+from math import pi, cos, sin
 
 def Pdraw(points, p1,p2,p3,img,c):
 	for i in points:
@@ -22,21 +24,43 @@ def Cdraw(p, img):
 	cv2.imshow('test', img)		#show the window test with the image img
 	cv2.waitKey(1000)
 
-#For sorting the x values in generated points
-def insertion_sort(InputList):
-    for i in range(1, len(InputList)):
-        j = i-1
-        nxt_element = InputList[i]
-		# Compare the current element with next one		
-        while (InputList[j] > nxt_element) and (j >= 0):
-            InputList[j+1] = InputList[j]
-            j=j-1
-        InputList[j+1] = nxt_element
+def partition(array, begin, end):
+    pivot = begin
+    for i in range (begin+1, end+1):
+        if array[i] <= array[begin]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[begin] = array[begin], array[pivot]
+    return pivot
 
-#Generate Top and Bottom hull start
+def quicksort(array, begin, end):   
+    def _quicksort(array, begin, end):
+        if begin >= end:
+            return
+        pivot = partition(array, begin, end)
+        _quicksort(array, begin, pivot-1)
+        _quicksort(array, pivot+1, end)
+    return _quicksort(array, begin, end)	
+	
+#For sorting the x values in generated points
+def qsort(InputList):
+	end = len(InputList)-1
+	InputList = quicksort(InputList,0,end)
+	
+	
+
+
+
+
+
+
+
+
+
+#Generate Top and Bottom hull startHull for NO ANIMATION
 def quickhull(points, p1, p2):
 
-	start = time.time()
+	startHull = time.time()
 	bbest = 0	#used to check if farthest point distance for comparison to bottom hull
 	tbest = 0	#used to check if farthest point distance for comparison to top hull
 	bmax = None #used to store farthest point for bottom hull
@@ -93,10 +117,10 @@ def quickhull(points, p1, p2):
 		hull.append(tmax)
 	else:
 		hull.append(p2)
-	print(time.time() -start)	
+	print(time.time() -startHull)	
 	return hull
 
-#Generate bottom convex hull	code is nearly identical to quickhull but only generates bottom hull
+#Generate bottom convex hull for NO ANIMATION	code is nearly identical to quickhull but only generates bottom hull
 def quickb(points,p1,p2):
 	bbest = 0
 	bmax = None
@@ -132,7 +156,7 @@ def quickb(points,p1,p2):
 	
 	return hull
 
-#Generate top convex hull code is nearly identical to quickhull but only generates top hull
+#Generate top convex hull for NO ANIMATION code is nearly identical to quickhull but only generates top hull
 def quickt(points,p1,p2):
 	tbest = 0
 	tmax = None
@@ -173,7 +197,7 @@ def quickt(points,p1,p2):
 	return hull	
 
 
-#Generate Top and Bottom hull start
+#Generate Top and Bottom hull startHull for Animation
 def aquickhull(points, p1, p2,img):
 
 	bbest = 0	#used to check if farthest point distance for comparison to bottom hull
@@ -342,8 +366,10 @@ def aquickt(points,p1,p2,img):
 
 
 
-def start(points,img,anim):
-
+def startHull(points,img,anim):
+	if points == None:
+		return
+		
 	if anim == 1:
 		hull = aquickhull(points,points[0],points[len(points)-1],img)
 		#color hull points red
@@ -362,7 +388,7 @@ def start(points,img,anim):
 		cv2.line(img,hull[len(hull) -1],hull[0],(0,255,0),1)	#connect the last line	
 		cv2.imshow('test', img)		#show the window test with the image img
 	else:
-		#generate quickhull feeding in sorted extremes as starting point
+		#generate quickhull feeding in sorted extremes as startHulling point
 		hull = quickhull(points,points[0],points[len(points)-1])
 	
 		
@@ -377,6 +403,8 @@ class gui(tk.Tk):
 		
 		tk.Tk.__init__(self)
 		self.title("Convex Hull")
+		
+		#code for shape choices in GUI
 		self.lable = tk.Label(self, text = "Select Data Point Shape").pack()
 		
 		choices = ['Star','Triangle','Circle','Square','Random']
@@ -387,24 +415,27 @@ class gui(tk.Tk):
 		self.list.bind('<<ListboxSelect>>', self.onselect)
 		self.list.pack()
 		
-		
+		#code for animate check button
 		self.var = tk.IntVar()
 		self.check = tk.Checkbutton(self, text="Animate", variable = self.var)
 		self.check.pack()
-		self.label2 = tk.Label(self, text = "Number of points to the power of 10").pack()
 		
+		#Code for number of points input in GUI		
+		self.label2 = tk.Label(self, text = "Number of points to the power of 10").pack()
+			
 		sv = tk.StringVar()
 		sv.trace("w", lambda name, index, mode, sv=sv: self.onselect(self))
-		
 		vcmd = (self.register(self.validate),'%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
 		self.datap = tk.Entry(self, validate = 'key', validatecommand = vcmd, textvariable = sv)
-		self.datap.insert(0,'1')
+		self.datap.insert(0,'2')
 		self.datap.pack()
 		
+		#Run button
 		self.button = tk.Button(self, text="Run", command = self.go).pack()
 
+	#Check if text entered is the values below, if so return true and print character in field
 	def validate(self, action, index, value_if_allowed, prior_value, text, validation_type, trigger_type, widget_name):
-		if text in '0123456789*-+':
+		if text in '0123456789':
 			return True
 		else:
 			return False
@@ -414,12 +445,15 @@ class gui(tk.Tk):
 			selection = int(self.list.curselection()[0])
 			anim = self.var.get()
 			numpoints = int(self.datap.get())
-			start(self.points,self.img,anim)
+			startHull(self.points,self.img,anim)
 
 		except:
 			print("Check for Valid Selections!")
 				
 	def onselect(self,t):
+		selection = int(self.list.curselection()[0])
+		numpoints = int(self.datap.get())
+		self.points, self.img = genpoints(selection,numpoints)
 		try:
 			selection = int(self.list.curselection()[0])
 			numpoints = int(self.datap.get())
@@ -427,30 +461,75 @@ class gui(tk.Tk):
 		except:
 			return
 		
-		
+def genpo(type,num):
+	xy = []
+	#could implement switch/enum
+	if type == 0:	#if random
+		xy.append([(random.randint(1,199), random.randint(1,199)) for k in range(10 ** num)])
+	elif type == 1: #square
+		xy.append([(25,25),(25,174),(174,25),(174,174)])	#gen square points
+		b = []
+		b.append([(random.randint(25,174), random.randint(25,174)) for k in range((10 ** num)-4)]) #gen inner points
+		xy[0].extend(b[0])
+
+	elif type == 2: #circle
+		n = int(10**num *.2)
+		if n < 6:
+			n = 6
+		r = 75
+		xy.append([(int(cos(2*pi/n*x)*r)+99,int(sin(2*pi/n*x)*r)+99) for x in range(n)])	# gen perim
+
+		for i in range ((10**num)- n):	#gen inner circl points
+			r = 75 * math.sqrt(random.random())
+			a = 2 * pi * random.random()
+			xy[0].extend([((int(r * cos(2* pi * a) ) + 99), int (r * sin(2* pi * a) + 99))])
+
+
+	elif type == 3: #triangle
+		xy.append([(24,174),(174,174),(99,24)])
+		for i in range ((10**num)- 3):	#gen inner circl points
+			r1 = random.uniform(0,0.8628)
+			x = 24 + xy[0][1][0] * r1
+			r2 = random.uniform(abs(((x - 24)/75) - 1),1)
+			y = 24 + 150 * r2
+
+			xy[0].extend([(int(x),int(y))])
+			
+	elif type == 4: #star
+		xy.append([(random.randint(1,199), random.randint(1,199)) for k in range(10 ** num)])
+	
+	return xy
 		
 def genpoints(type, num):
-	#Generate random points
-	data = []
-	data.append([(random.randint(1,199), random.randint(1,199)) for k in range(10 ** num)])
+	if num < 7:
+		#Generate random points
+		data = genpo(type,num)
+		
+		#sort the random points
+		qsort(data[0])
+		
+		#generate a 200 by 200 pixel image matrix with 3 values for color
+		img = np.zeros([200,200,3])
 
-	#sort the random points
-	insertion_sort(data[0])
+		data.append(img)
 	
-	#generate a 200 by 200 pixel image matrix with 3 values for color
-	img = np.zeros([200,200,3])
-	data.append(img)
 	
-	#color points green
-	for i in data[0]:
-		img[i[1],i[0]] = (0,255,0)
+		#color points green
+		for i in data[0]:
+			img[i[1],i[0]] = (0,255,0)
 		
-	cv2.namedWindow('test', 0)	#make a window named test
-	cv2.imshow('test', img)		#show the window test with the image img
-	
-		
+		img = cv2.resize(img, (400,400))
+		cv2.namedWindow('test', 0)	#make a window named test
+		cv2.imshow('test', img)		#show the window test with the image img
+	else:
+		cv2.namedWindow('test', 0)	#make a window named test
+		cv2.imshow('test', img)		#show the window test with the image img
+		data = None
+	img = cv2.resize(img, (400,400))
 	return data
 	
+
+
 #main for testing quickhull
 if __name__ == "__main__":
 	t = gui()
